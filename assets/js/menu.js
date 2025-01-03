499cdd84542f2eab0969105b342f3cce4e8f8d93
Pawar Pratima Anil <papawar371123@kkwagh.edu.in>
2025-01-03 17:11:17 +0000

    updated a little

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,28 +1,28 @@
 // Products data for different sections
 const sections = {
     "drinks-cards": [
-        { name: "Citrus Juice", price: 5.99, img: "../assets/img/citrusjuice.jpg", info: "Freshly pressed citrus fruits served with ice and mint" },
-        { name: "Iced Tea", price: 4.50, img: "../assets/img/icedtea.jpg", info: "Iced tea with refreshing mint" },
-        { name: "Beer", price: 5.99, img: "../assets/img/beer.jpg", info: "Iced cold beer" },
-        { name: "Red Wine", price: 4.50, img: "../assets/img/redwine.jpg", info: "Roses are red and so as this wine." }
+        { name: "Citrus Juice", price: 200, img: "../assets/img/citrusjuice.jpg", info: "Freshly pressed citrus fruits served with ice and mint" },
+        { name: "Iced Tea", price: 150, img: "../assets/img/icedtea.jpg", info: "Iced tea with refreshing mint" },
+        { name: "Beer", price: 700, img: "../assets/img/beer.jpg", info: "Iced cold beer" },
+        { name: "Red Wine", price: 1200, img: "../assets/img/redwine.jpg", info: "Roses are red and so as this wine." }
     ],
     "special-cards": [
-        { name: "Grilled Beef Steak", price: 25.99, img: "../assets/img/grilledbeefsteak.jpg", info: "Grilled tender beef steak cut served with restaurant's special sauce" },
-        { name: "Beef Steak", price: 30.50, img: "../assets/img/beefsteak.jpg", info: "Medium rare beef steak served with grilled vegetables and spices" },
-        { name: "Grilled Pork Ribs", price: 25.99, img: "../assets/img/grilledporksteak.jpg", info: "Charred pork ribs smothered with barbecue sauce and served with grilled corn and French fries" },
-        { name: "Grilled Chicken Steak", price: 30.50, img: "../assets/img/grilledchickensteak.jpg", info: "Grilled chicken breast with lemon, tomato, chili, and carrots" }
+        { name: "Grilled Beef Steak", price: 450, img: "../assets/img/grilledbeefsteak.jpg", info: "Grilled tender beef steak cut served with restaurant's special sauce" },
+        { name: "Beef Steak", price: 750, img: "../assets/img/beefsteak.jpg", info: "Medium rare beef steak served with grilled vegetables and spices" },
+        { name: "Grilled Pork Ribs", price: 550, img: "../assets/img/grilledporksteak.jpg", info: "Charred pork ribs smothered with barbecue sauce and served with grilled corn and French fries" },
+        { name: "Grilled Chicken Steak", price: 250, img: "../assets/img/grilledchickensteak.jpg", info: "Grilled chicken breast with lemon, tomato, chili, and carrots" }
     ],
     "desserts-cards": [
-        { name: "BlueBerry cheesecake", price: 15.75, img: "../assets/img/blueberrycake.jpg", info: "Blueberry cheesecake with buttery graham crust" },
-        { name: "Red Velvet cake", price: 10.00, img: "../assets/img/redvelvetcake.jpg", info: "Double layered red velvet topped with dark chocolate" },
-        { name: "Brownies", price: 15.75, img: "../assets/img/brownies.jpg", info: "Chocolate brownies with alcapone nuts" },
-        { name: "Apple pie with cinnamon", price: 10.00, img: "../assets/img/applepie.jpg", info: "Mini apple pie with sweet cinnamon filling" }
+        { name: "BlueBerry cheesecake", price: 1200, img: "../assets/img/blueberrycake.jpg", info: "Blueberry cheesecake with buttery graham crust" },
+        { name: "Red Velvet cake", price: 950, img: "../assets/img/redvelvetcake.jpg", info: "Double layered red velvet topped with dark chocolate" },
+        { name: "Brownies", price: 570, img: "../assets/img/brownies.jpg", info: "Chocolate brownies with alcapone nuts" },
+        { name: "Apple pie with cinnamon", price: 750, img: "../assets/img/applepie.jpg", info: "Mini apple pie with sweet cinnamon filling" }
     ],
     "salad-cards": [
-        { name: "Greek Salad", price: 12.99, img: "../assets/img/greeksalad.jpg", info: "Greek salad with tomato and cucumber, with red onion and feta cheese, sprinkled with black olives." },
-        { name: "TFL Salad", price: 10.99, img: "../assets/img/TFLsalad.jpg", info: "Cherry tomato salad with fresh lettuce, bite sized cheese and mustard garlic oil." },
-        { name: "Veggie Lemon Salad", price: 12.99, img: "../assets/img/veggielemonsalad.jpg", info: "Fresh vegetable salad with citrusy lemon slices and cherry tomato" },
-        { name: "Fresh Chicken Salad", price: 10.99, img: "../assets/img/chickensalad.jpg", info: "Cherry tomato salad with fresh lettuce, bite sized cheese and mustard garlic oil." }
+        { name: "Greek Salad", price: 450, img: "../assets/img/greeksalad.jpg", info: "Greek salad with tomato and cucumber, with red onion and feta cheese, sprinkled with black olives." },
+        { name: "TFL Salad", price: 250, img: "../assets/img/TFLsalad.jpg", info: "Cherry tomato salad with fresh lettuce, bite sized cheese and mustard garlic oil." },
+        { name: "Veggie Lemon Salad", price: 350, img: "../assets/img/veggielemonsalad.jpg", info: "Fresh vegetable salad with citrusy lemon slices and cherry tomato" },
+        { name: "Fresh Chicken Salad", price: 550, img: "../assets/img/chickensalad.jpg", info: "Cherry tomato salad with fresh lettuce, bite sized cheese and mustard garlic oil." }
     ]
 };
 
@@ -37,7 +37,7 @@ function generateCards(sectionId, products) {
                 <img src="${product.img}" alt="${product.name}" />
                 <div class="product-card-content">
                     <h4>${product.name}</h4>
-                    <p>Price: $${product.price.toFixed(2)}</p>
+                    <p>INR ${product.price.toFixed(2)}</p>
                     <button onclick="showPopup(${index}, '${sectionId}')">Add to Cart</button>
                 </div>
             </div>
@@ -111,4 +111,13 @@ function addToBag(productName, pricePerUnit) {
 // Generate cards for each section
 Object.keys(sections).forEach(sectionId => {
     generateCards(sectionId, sections[sectionId]);
+});
+
+
+window.addEventListener("load", () => {
+    const preloader = document.getElementById("preloader");
+    const content = document.getElementById("content");
+
+    preloader.style.display = "none"; // Hide the preloader
+    content.style.display = "block"; // Show the content
 });
